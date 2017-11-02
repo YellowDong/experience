18eea43d227c6bee1f10c627b931ee02654114a8
hezudao <787773580@qq.com>
2017-11-02 14:25:23 +0800

    update FinancialNews

--- FinancialNews/spider.py
+++ FinancialNews/spider.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 #  _*_coding:utf8_*_
 
 import requests
@@ -6,15 +6,18 @@ from urllib3.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 from lxml import etree
 import jieba
-import operator
+import sys
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import os
+from PIL import Image
+import numpy as np
 
+jieba.load_userdict("dictNew.txt")
 
 
 def get_html(url):
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
     try:
         resp = requests.get(url, headers=headers)
         if resp.status_code == 200:
@@ -27,49 +30,55 @@ def get_title(html):
     titlelist = ''
     if html:
         soup = BeautifulSoup(html, 'lxml')
-
         title = soup.find_all('table', attrs={'class': '12v'})
         for i in title:
             titlelist += i.a.text.strip()
         return titlelist
 
 
+def get_link(html):
+    if html:
+        soup = BeautifulSoup(html, 'lxml')
+        links = soup.find_all('table', attrs={'class': '12v'})
+        for i in links:
+            link = i.a['href'].strip()
+            yield link
 
-def main():
-    alltitle = ''
-    for page in range(11):
+
+def get_detail(html):
+    if html:
+        doc = etree.HTML(html)
+        plist = doc.xpath('//div[@class="left_zw"]/p/text()')
+        return plist
+
+
+def main(keword, page):
+    allcontent = ''
+    for page in range(page):
         url = 'http://channel.chinanews.com/cns/cl/fortune-lccp.shtml?pager={0}'.format(page)
         html = get_html(url)
-        data = get_title(html)
-        alltitle += data
-    datalist = jieba.cut(alltitle)
+        if keword == 'title':
+            data = get_title(html)
+            allcontent += data
+        else:
+            link = get_link(html)
+            for i in link:
+                html = requests.get(i).content
+                allcontent = ''.join(get_detail(html))
+    datalist = jieba.cut(allcontent, cut_all=False)
     haha = " ".join(datalist)
-    print(haha)
-    font = '/usr/share/fonts/'
+    font = os.path.join(os.path.dirname(__file__), "DroidSansFallbackFull.ttf") #wget http://labfile.oss.aliyuncs.com/courses/756/DroidSansFallbackFull.ttf下载中文词库
+    png_font = np.array(Image.open(os.path.join(os.path.dirname(__file__), "love.png")))
     wc = WordCloud(font_path=font,
-                   background_color="white", max_words=300,
-                   max_font_size=40, random_state=42)
+                   background_color="black", max_words=400, mask=png_font,
+                   max_font_size=60, random_state=30)
 
     my_wordcloud=wc.generate(haha)
-    #my_wordcloud = WordCloud().generate(haha)
     plt.imshow(my_wordcloud)
     plt.axis("off")
     plt.show()
 
-    dic = {}
-    for ele in datalist:
-        if ele not in dic:
-            dic[ele] = 1
-        else:
-            dic[ele] = dic[ele] + 1
-    sorted_word = sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
-
-    line = ''
-    for ele in sorted_word:
-        if (len(ele[0]) > 1):  # 只顯示一個字以上的詞，如需顯示一個字的詞請註解掉此行
-            line += ele[0] + ' ' + str(ele[1]) + '\n'
-    print(line)
-
 
 if __name__ == '__main__':
-    main()
+    keyword, page = str(sys.argv[1]), int(sys.argv[2])
+    main(keyword, page)
