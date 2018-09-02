bf768957ced456c8ffe02e898400b5491f8924f6
dongshao <helovecandy@gmail.com>
2018-09-03 07:12:18 +0800

    update settings.py about static and host

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k#j0znqd=m0k74gz-*!$lazg*w$b(=3vc4ky7!q^+t1i)w^w_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.xiaoyunliu.pro']
 
 
 # Application definition
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
